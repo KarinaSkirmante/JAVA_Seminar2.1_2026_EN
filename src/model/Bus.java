e45de13a22806bd67d322d1ea561146236ad3007
KarinaSkirmante <karina.krinkele@gmail.com>
2026-03-27 15:00:29 +0200

    Edit: added set functions, constructors and toString in the Bus class

--- src/model/Bus.java
+++ src/model/Bus.java
@@ -15,9 +15,47 @@ public class Bus extends Vehicle{
 	}
 	
 	//3. setters
+	public void setNumberOfSeats(int inputNumberOfSeats) {
+		if(inputNumberOfSeats > 5 && inputNumberOfSeats < 100) {
+			numberOfSeats = inputNumberOfSeats;
+		}
+		else
+		{
+			numberOfSeats = 5;
+		}
+	}
+	
+	public void setHasBaggageDivision(boolean inputHasBaggageDivision) {
+		hasBaggageDivision = inputHasBaggageDivision;
+	}
+	
+	
 	
 	//4. both constructors
+	public Bus() {
+		super();//this will call Vehicle() no args constructor
+		setTitle("MB bus");//I would like to change default vehicle name to that
+		setVehicleCode();//need to call it because the title is changed
+		setNumberOfSeats(50);
+		setHasBaggageDivision(true);
+	}
+	
+	public Bus(String inputTitle, float inputPrice,
+			EnergyType inputEtype, int inputNumberOfSeats,
+			boolean inputHasBaggageDivision) {
+		super(inputTitle, inputPrice, inputEtype);
+		//^^^ this will call argument constructor from base class -> Vehicle(String inputTitle, float inputPrice, EnergyType inputEtype)
+		setNumberOfSeats(inputNumberOfSeats);
+		setHasBaggageDivision(inputHasBaggageDivision);
+		
+	}
+	
 	
 	//5. toString
+	public String toString() {
+		String result = super.toString() + " " 
+	+ numberOfSeats + " " + hasBaggageDivision;
+		return result;
+	}
 	 
 }
