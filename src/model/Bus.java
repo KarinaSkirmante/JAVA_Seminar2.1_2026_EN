1d3f92f3a5277bf3bdfd6224cd6940f097b882fd
KarinaSkirmante <karina.krinkele@gmail.com>
2026-03-27 14:29:27 +0200

    Add: added Bus class with varaibales and getters

---
+++ src/model/Bus.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Bus extends Vehicle{
+	//1. variables 
+	private int numberOfSeats;
+	private boolean hasBaggageDivision;
+	//2. getters
+	
+	public int getNumberOfSeats() {
+		return numberOfSeats;
+	}
+	public boolean getHasBaggageDivision()
+	{
+		return hasBaggageDivision;
+	}
+	
+	//3. setters
+	
+	//4. both constructors
+	
+	//5. toString
+	 
+}
