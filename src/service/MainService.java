42c95a30a3d08331cf5760b4e646a96dc04be2e8
KarinaSkirmante <karina.krinkele@gmail.com>
2026-03-27 15:22:56 +0200

    Edit: added som bus objects in the MainService

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -1,5 +1,6 @@
 package service;
 
+import model.Bus;
 import model.EnergyType;
 import model.Vehicle;
 
@@ -10,6 +11,12 @@ public class MainService {
 		System.out.println(v1);
 		Vehicle v2 = new Vehicle("Bicycle", 99.66f, EnergyType.electric);
 		System.out.println(v2);
+		
+		Bus b1 = new Bus();
+		System.out.println(b1);
+		Bus b2 = new Bus("Ecoline", 10000.99f,EnergyType.diesel,
+				70, true);
+		System.out.println(b2);
 
 	}
 
